4fac53245790597b0aad258112c77e74fae47a83
lhk3337 <lhk3337@naver.com>
2024-10-10 15:43:57 +0900

    server init and mysql conn

---
+++ backend/db/mysql.js
@@ -0,0 +1,21 @@
+var mysql = require("mysql2");
+require("dotenv").config();
+const db_info = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME,
+};
+
+module.exports = {
+  init: function () {
+    return mysql.createConnection(db_info);
+  },
+  connect: function (conn) {
+    conn.connect(function (err) {
+      if (err) console.error("mysql connection error : " + err);
+      else console.log("mysql is connected successfully!");
+    });
+  },
+};
---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const db = require("./db/mysql.js");
+
+const app = express();
+const conn = db.init();
+const PORT = process.env.URL_PORT;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.set("port", process.env.PORT || 3000); // 포트 설정
+app.set("host", process.env.HOST || "0.0.0.0"); // 아이피 설정
+
+app.get("/", function (req, res) {
+  const sql = process.env.ALL_QUERY;
+  conn.query(sql, function (err, result) {
+    if (err) console.log("query is not excuted: " + err);
+    else res.send(result);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
